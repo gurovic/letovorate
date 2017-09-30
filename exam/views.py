51ecb8e714ade451f6d50de5fd63a81697224702
Vladimir Gurovic <gurovic@gmail.com>
2017-09-30 21:11:43 +0300

    create group form

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Examiner, Subject, Round
+
+def new_exam(request):
+    return render(request, 'exam/new_exam.html', 
+                       {'subjects': Subject.objects.all(),
+                        'rounds': Round.objects.all(),
+                        'range50': list(range(50)),
+                       })
+
+def create(request):
+    pass
