569c33fff5efd1c4a391b53bb2142011c510d463
Vladimir Gurovic <gurovic@gmail.com>
2017-09-30 18:49:47 +0300

    models upgraded

--- exam/admin.py
+++ exam/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Examiner
+from .models import Examiner, Subject, Round
 
 admin.site.register(Examiner)
+admin.site.register(Subject)
+admin.site.register(Round)
--- exam/models.py
+++ exam/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
+class Subject(models.Model):
+    title = models.CharField(max_length=200,verbose_name="Название")
+
+    def __str__(self):
+        return self.title
+
+class Round(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название")
+
+    def __str__(self):
+        return self.title
+
 class Examiner(models.Model):
-    fio = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    fio = models.CharField(max_length=200, verbose_name="Фамилия, имя, отчество")
+    email = models.CharField(max_length=200, verbose_name="E-mail")
+    subject = models.ForeignKey(Subject, verbose_name="Предмет")
+    round = models.ForeignKey(Round, verbose_name="Раунд")
+    grade = models.IntegerField(verbose_name="Класс")
+    code = models.CharField(max_length=6, verbose_name="Код доступа")
+    grade = models.IntegerField(verbose_name="Класс")
+
+    def __str__(self):
+        return self.fio
